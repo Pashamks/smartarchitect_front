c8e270f2147a4ca098a23dd96ce5f12ad733532b
p.somko.lionwood@gmail.com <p.somko.lionwood@gmail.com>
2024-05-27 19:16:38 +0300

    changed router

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
 import App from './App';
 import { AuthProvider } from "./Components/Authentication/AuthProvider";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
